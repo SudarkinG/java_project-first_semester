e5dc234b33926ca8fddd617132403f0aeb65a51d
sudarkingeorgiy <sudarkin.gi@phystech.edu>
2026-02-28 14:00:09 +0300

    add BeanPostProcessor and LoggingAspect

---
+++ src/main/java/com/mipt/sudarkingeorgiy/aspect/TaskLifecycleProcessor.java
@@ -0,0 +1,33 @@
+package com.mipt.sudarkingeorgiy.aspect;
+
+import com.mipt.sudarkingeorgiy.repository.TaskRepository;
+import com.mipt.sudarkingeorgiy.service.TaskService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+/** BeanPostProcessor пишет в лог до и после инициализации бинов TaskService и TaskRepository */
+@Component
+public class TaskLifecycleProcessor implements BeanPostProcessor {
+
+    private static final Logger logger = LoggerFactory.getLogger(TaskLifecycleProcessor.class);
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof TaskService || bean instanceof TaskRepository) {
+            logger.info("Before initialization of bean '{}' of type {}", beanName, bean.getClass().getSimpleName());
+        }
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof TaskService || bean instanceof TaskRepository) {
+            logger.info("After initialization of bean '{}' of type {}", beanName, bean.getClass().getSimpleName());
+        }
+        return bean;
+    }
+}
+
