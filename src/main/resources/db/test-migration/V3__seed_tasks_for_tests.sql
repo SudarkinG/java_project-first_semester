8a0954615c558922b2c43b31c20e8e3325a9b685
sudarkingeorgiy <sudarkin.gi@phystech.edu>
2026-04-08 23:37:16 +0300

    add migration and flyway, upd prod

---
+++ src/main/resources/db/test-migration/V3__seed_tasks_for_tests.sql
@@ -0,0 +1,4 @@
+INSERT INTO tasks (id, title, description, completed, created_at, due_date, priority, tags, updated_at)
+VALUES
+    (10, 'Seed 1', NULL, FALSE, CURRENT_TIMESTAMP, NULL, 'LOW', NULL, CURRENT_TIMESTAMP),
+    (11, 'Seed 2', NULL, TRUE, CURRENT_TIMESTAMP, NULL, 'HIGH', NULL, CURRENT_TIMESTAMP);
